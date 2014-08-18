82b3fc1c03a4bcf1c1fe38076aa77afa8a1eee7d
Michael Sexton <mick.sexton@gmail.com>
2014-08-18 14:47:23 +1000

    Added ngram functionality

--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
-MINE_NAME_KEYWORDS = ["project","mine","deposit","gold","area","ore","body"]
+MINE_NAME_KEYWORDS = ["project","mine","deposit","gold","area","ore","body","wall"]
 
 
 require 'rubygems'
 require  'safe_attributes/base'
 gem 'activerecord'
 require 'yaml'
+require 'ruby_nlp/ngram'
 require 'active_record'
 require 'csv'
 
@@ -19,51 +20,30 @@ def read_borehole_attributes
   borehole_atrributes = MineralsEntityAttribute.deposit_find
 
   borehole_atrributes.each do |ba|
-    name = ba.text_value
-    check_deposit_name(name)
+    text = Ngram.new(ba.text_value)
+    check_deposit_name(text)
   end
   
 end
 
-def check_deposit_name(name)
-  ACRONYMS.keys.include?(name) ? name = ACRONYMS[name] : 
-  if name.length < 3
-    puts "Name '#{name}' too short to query"
-    return 
-  end
-  if GENERIC_KEYWORDS.include?(name)
-    puts "Name '#{name}' too generic to query"
-    return 
-  end
-  puts "Looking for company with name \"#{name}\" ..."
-  companies=Company.by_name(name)
-  case companies.size
-  when 0
-    companies = Array.new
-    if name.include?("JV")
-      puts "Company #{name} is a joint venture"
-      name = name.split(/JV/).map{|s| s.strip}
-    else
-      name = name.split(/ /) 
+def check_deposit_name(text,n=1)
+
+  ngrams = text.ngrams(n)
+  
+  ngrams.each do |ng|
+    case n
+    when 1
+      name = ng[0]
+      deposits=Deposit.by_name(name)
+      case deposits.size
+      when 1
+      else 
+        check_deposit_name(text,2)
+      end  
+    when 2
+      name = "#{ng[0]} #{ng[1]}"
+      deposits=Deposit.by_name(name)
     end
-    if name.size > 1
-      name.each do |n|
-        companies << check_company_name(n)
-      end
-    else
-      puts "No companies found"
-      return companies
-    end
-    return companies.compact # compact removes nils returned from recursive searches
-  when 1
-    puts "Found #{companies.first.company_name} for #{name}"
-    return companies
-  else
-    puts "Company: #{name} returns two or more companies"
-    companies.each_with_index do |company,i|
-      puts "(#{i}) #{company.company_name}"
-    end
-    return companies
   end
   
 end
